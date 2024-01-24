dee7794e221c54f17dbd0bb4c1ac659791968a2a
David Pal <david.pal@instacart.com>
2024-01-24 09:21:06 -0500

    Fix code

--- init_file_checker.py
+++ init_file_checker.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
-"""TODO: Add docstring."""
+"""Checker of __init__.py files.
+
+Author: David Pal <davidko.pal@gmail.com>
+Date: 2024
+
+Usage:
+
+   python init_file_checker.py [OPTIONS] [FILES ...]
+"""
 
 from __future__ import annotations
 
@@ -25,57 +33,59 @@ def parse_command_line() -> argparse.Namespace:
     """Parses command line arguments."""
     parser = argparse.ArgumentParser(
         prog="init-file-checker",
-        description="Linter and formatter for source code files and text files",
+        description="Checker of __init__.py files in Python projects",
         allow_abbrev=False,
         add_help=True,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
         "--add-missing",
-        help="Do not format files. Only report which files would be formatted.",
+        help="Add missing __init__.py files.",
         required=False,
         action="store_true",
         default=False,
     )
-    parser.add_argument("input_files", help="List of input files", nargs="+", default=[], type=str)
+    parser.add_argument(
+        "input_directories", help="List of input directories", nargs="+", default=[], type=str
+    )
     parsed_arguments = parser.parse_args()
     return parsed_arguments
 
 
-def find_all_python_files_recursively(file_name: str) -> List[str]:
+def find_all_python_files_recursively(path: str) -> List[str]:
     """Finds all *.py files in directories recursively."""
-    if pathlib.Path(file_name).is_file() and file_name.endswith(".py"):
-        return [file_name]
+    if pathlib.Path(path).is_file() and path.endswith(".py"):
+        return [path]
 
-    if pathlib.Path(file_name).is_dir():
+    if pathlib.Path(path).is_dir():
         return [
             expanded_file_name
-            for inner_file in sorted(pathlib.Path(file_name).iterdir())
+            for inner_file in sorted(pathlib.Path(path).iterdir())
             for expanded_file_name in find_all_python_files_recursively(str(inner_file))
         ]
 
     return []
 
 
-def find_all_python_files(file_names: List[str]) -> List[str]:
-    """Finds all *.py files in directories recursively."""
-    return [
-        expanded_file_name
-        for file_name in file_names
-        for expanded_file_name in find_all_python_files_recursively(file_name)
-    ]
-
-
-def parent_directories(file_name: str) -> List[str]:
-    """List parent directories of a relative or absolute path of a file."""
+def parent_directories(file_name: str, base_directory: str) -> List[str]:
+    """List parent directories of file starting with a base directory."""
     parts = file_name.split("/")
-    root_prefix = "/" if file_name.startswith("/") else ""
-    return [root_prefix + "/".join(parts[:i]) for i in range(len(parts))]
+    directories = []
+    for i in range(len(parts)):
+        directory = "/".join(parts[:i])
+        if directory.startswith(base_directory):
+            directories.append(directory)
+    return directories
 
 
-def find_all_directories(file_names: List[str]) -> Set[str]:
+def find_all_subdirectories(file_names: List[str], base_directory: str) -> Set[str]:
     """Finds missing '__init__.py' files."""
-    return {directory for file_name in file_names for directory in parent_directories(file_name)}
+    print(f"file_names = {file_names}, base_directory='{base_directory}' ... ")
+    return {
+        directory
+        for file_name in file_names
+        for directory in parent_directories(file_name, base_directory)
+    }
 
 
 def find_missing_init_files(directories: Iterable[str]) -> List[str]:
@@ -101,10 +111,16 @@ def create_missing_init_files(file_names: List[str]):
 def main():
     """Finds missing '__init__.py' files and, optionally, adds the missing files."""
     parsed_arguments = parse_command_line()
-    file_names = find_all_python_files(parsed_arguments.input_files)
-    directories = find_all_directories(file_names)
-    print(directories)
-    missing_files = find_missing_init_files(directories)
+
+    directories_to_check: Set[str] = set()
+    for input_directory in parsed_arguments.input_directories:
+        base_directory = str(pathlib.Path(input_directory).resolve())
+        if not pathlib.Path(base_directory).is_dir():
+            die(2, f"'{input_directory}' is not a directory.")
+        python_files = find_all_python_files_recursively(base_directory)
+        for directory in find_all_subdirectories(python_files, base_directory):
+            directories_to_check.add(directory)
+    missing_files = find_missing_init_files(directories_to_check)
     if missing_files and not parsed_arguments.add_missing:
         die(1)
     if parsed_arguments.add_missing:
